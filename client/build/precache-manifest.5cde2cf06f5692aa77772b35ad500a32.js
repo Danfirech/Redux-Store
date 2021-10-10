ddb10cfcf1e0829fdfe62f0894de12a6bba0763f
Dan Possehl <danielbpossehl@gmail.com>
2021-10-10 16:51:59 -0600

    ran buiild

---
+++ client/build/precache-manifest.5cde2cf06f5692aa77772b35ad500a32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e183eb1ba5ff85061e0f83353ce18439",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a19e93ba5bd5330b49f",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "2facffb1479b7456c481",
+    "url": "/static/js/2.873ae7ec.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.873ae7ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a19e93ba5bd5330b49f",
+    "url": "/static/js/main.062e8104.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
